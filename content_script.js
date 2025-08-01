979cec3995f62690d3a84d5e3c7ef6b0c49b8cbc
Bob McLarnon <bob.mclarnon@gmail.com>
2025-08-01 23:56:00 +0100

    replaced content_script.js

--- content_script.js
+++ content_script.js
@@ -1,42 +1,15 @@
-const FONT_ID = 'dotsies-font-style';
-const FONT_CSS = `
-@font-face {
-  font-family: 'Dotsies';
-  src: url('${browser.runtime.getURL('fonts/dotsies.otf')}') format('opentype');
-  font-weight: normal;
-  font-style: normal;
-}
-.dotsies-on *:not(script):not(style) {
-  font-family: 'Dotsies', sans-serif !important;
-}
+const fontUrl = chrome.runtime.getURL("fonts/dotsies.otf");
+
+const style = document.createElement("style");
+style.textContent = `
+  @font-face {
+    font-family: 'Dotsies';
+    src: url('${fontUrl}') format('opentype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  body, body *:not([class*="icon"]):not([class*="emoji"]) {
+    font-family: 'Dotsies' !important;
+  }
 `;
-
-function setDotsies(enabled) {
-  let styleTag = document.getElementById(FONT_ID);
-  if (enabled) {
-    if (!styleTag) {
-      styleTag = document.createElement('style');
-      styleTag.id = FONT_ID;
-      styleTag.textContent = FONT_CSS;
-      document.head.appendChild(styleTag);
-    }
-    document.body.classList.add('dotsies-on');
-  } else {
-    if (styleTag) styleTag.remove();
-    document.body.classList.remove('dotsies-on');
-  }
-}
-
-// Listen for toggle message from background script
-browser.runtime.onMessage.addListener((message) => {
-  if (message && message.type === "TOGGLE_DOTSIES") {
-    setDotsies(message.enabled);
-  }
-});
-
-// On page load, check storage and set font if enabled
-(async () => {
-  let { dotsiesSites = {} } = await browser.storage.sync.get("dotsiesSites");
-  const domain = window.location.hostname;
-  setDotsies(!!dotsiesSites[domain]);
-})();
+document.head.appendChild(style);
